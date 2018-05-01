83487809af0f13fa216b33cf8e8f804f0479bd18
Marek Pinto <marek.pinto@gmail.com>
2018-04-30 21:43:06 -0700

    Updated and Renamed Code

---
+++ nedcPrototype.ino
@@ -0,0 +1,29 @@
+//NOTE: This Code has not yet been fully completed and still does not include all functionality.
+
+#include <Window.h>
+
+Window firstWindow(2,3);
+Window secondWindow(10,11);
+Window thirdWindow(0,1);
+Window fourthWindow(12,13);
+void setup() {
+  
+
+}
+
+void loop() {
+  
+  firstWindow.openWindow();
+  //secondWindow.openWindow();
+  thirdWindow.openWindow();
+  fourthWindow.openWindow();
+  delay(5000);
+  firstWindow.closeWindow();
+  secondWindow.closeWindow();
+  thirdWindow.closeWindow();
+  fourthWindow.closeWindow();
+  delay(5000);
+  
+}
+
+
